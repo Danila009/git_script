f809275ee8cad90808c8d43f2e59b8db5bfc882e
210User <210User@pgk63.ru>
2023-03-07 13:37:18 +0400

    push repos from cfif31 to github

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
 import base64
+import pathlib
 import subprocess
 
 import requests
+from cd import cd
+
+base_path = pathlib.Path().resolve()
 
 
 def download_repos(username: str, password: str, limit: int):
     url = f'https://cfif31.ru:3000/api/v1/users/{username}/repos?limit={limit}'
 
-    token = base64.b64encode(bytes(f'{username}:{password}', 'utf-8'))
+    token = get_token(username, password)
 
     response = requests.get(url, headers={
         'Authorization': f'Basic {token.decode("utf-8")}'
@@ -15,9 +19,57 @@ def download_repos(username: str, password: str, limit: int):
 
     json = response.json()
 
-    subprocess.run(['mkdir', f'repos_{username}'])
+    subprocess.run(['mkdir', f'repos_{username}'], shell=True)
 
-    for repos in json:
-        print('-------------------------------')
-        clone_url = repos['clone_url']
-        subprocess.run(['git', 'clone', clone_url])
+    with cd(f'{base_path}\\repos_{username}'):
+        for repos in json:
+            print('-------------------------------')
+            clone_url = repos['clone_url']
+            subprocess.run(['git', 'clone', clone_url], shell=True)
+
+
+def push_repos(fromUsername, fromPassword, toUsername: str, token_github: str, limit: int):
+    url = f'https://cfif31.ru:3000/api/v1/users/{fromUsername}/repos?limit={limit}'
+
+    from_token = get_token(fromUsername, fromPassword)
+
+    get_repos_response = requests.get(url, headers={
+        'Authorization': f'Basic {from_token.decode("utf-8")}'
+    })
+
+    repos_json = get_repos_response.json()
+
+    subprocess.run(['mkdir', f'repos_{fromUsername}'], shell=True)
+
+    with cd(f'{base_path}\\repos_{fromUsername}'):
+        for repos in repos_json:
+            clone_url = repos['clone_url']
+            repos_name = repos['name']
+            print('-------------------------------')
+            subprocess.run(['git', 'clone', clone_url], shell=True)
+            create_repos(toUsername, token_github, repos_name)
+
+            with cd(f'{base_path}\\repos_{fromUsername}\\{repos_name}'):
+                subprocess.run(['git', 'remote', 'remove', 'origin'], shell=True)
+                subprocess.run(['git', 'remote', 'add', 'origin', f'https://github.com/{toUsername}/{repos_name}.git'], shell=True)
+                subprocess.run(['git', 'push', '-u', 'origin', 'master'], shell=True)
+                subprocess.run(['cd', f'{base_path}\\repos_{fromUsername}'], shell=True)
+
+
+def create_repos(username: str, token_github, reposName: str):
+    url = f'https://api.github.com/user/repos'
+
+    response = requests.post(url, headers={
+        'Authorization': f'Bearer {token_github}'
+    }, json={
+        'name': reposName
+    })
+
+    print("------------------")
+    print(response.url)
+    print(response)
+    print("------------------")
+
+
+def get_token(username: str, password: str) -> bytes:
+    return base64.b64encode(bytes(f'{username}:{password}', 'utf-8'))
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from api import download_repos
+from api import download_repos, push_repos
 
 print('git username')
-username = input()
+from_username = input()
 print('git password')
-password = input()
+from_password = input()
 print('limit repository')
 limit = int(input())
+print('push github repos [y/n]')
+cloneRepos = input()
 
-download_repos(username, password, limit)
+if cloneRepos == 'y':
+    print('github username')
+    to_username = input()
+    print('github token')
+    token_github = input()
 
+    push_repos(from_username, from_password, to_username, token_github, limit)
+else:
+    download_repos(from_username, from_password, limit)
